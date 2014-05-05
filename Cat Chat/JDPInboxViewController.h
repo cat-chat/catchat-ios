a4a2e32b32a856f67ecfcb6ee3a0f6288d5e8831
Joel Parsons <joel@brightfire.eu>
2014-05-05 14:02:51 +0100

    added inbox view controller and rejigged navigation

---
+++ Cat Chat/JDPInboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  JDPInboxViewController.h
+//  Cat Chat
+//
+//  Created by Joel Parsons on 05/05/2014.
+//  Copyright (c) 2014 Joel Parsons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDPInboxViewController : UITableViewController
+
+@end
