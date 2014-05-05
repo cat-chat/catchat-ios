9ac6101b3e2af1153b635febd4709347332ed484
Joel Parsons <joel@brightfire.eu>
2014-05-05 12:04:42 +0100

    added constants to project

---
+++ Constants/JDPConstants.h
@@ -0,0 +1,13 @@
+//
+//  JDPConstants.h
+//  Cat Chat
+//
+//  Created by Joel Parsons on 05/05/2014.
+//  Copyright (c) 2014 Joel Parsons. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JDPConstants : NSObject
+
+@end
