fa418d3a6955205c698b9bfd493a54f9919d2d0f
Joel Parsons <joel@brightfire.eu>
2014-05-10 10:31:22 +0100

    added cell for displaying cat thumbnail images

---
+++ Cat Chat/JDPCatThumbnailCell.h
@@ -0,0 +1,14 @@
+//
+//  JDPCatThumbnailCell.h
+//  Cat Chat
+//
+//  Created by Joel Parsons on 05/05/2014.
+//  Copyright (c) 2014 Joel Parsons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDPCatThumbnailCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
